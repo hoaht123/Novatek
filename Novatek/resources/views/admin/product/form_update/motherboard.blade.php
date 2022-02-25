ff0c5a3558dc404a9c6c4597b248f0210776cf3b
Van Tinh <cftq0007@gmail.com>
2022-02-25 20:00:27 +0700

    up

---
+++ Novatek/resources/views/admin/product/form_update/motherboard.blade.php
@@ -0,0 +1,124 @@
+@extends('admin.admin_layout')
+@section('admin-content')
+        <div class="dropdown show ">
+            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Choose component
+            </a>
+          
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                <a class="dropdown-item" href="{{route('create_motherboard')}}">Motherboard</a>
+                <a class="dropdown-item" href="{{route('create_cpu')}}">CPU</a>
+                <a class="dropdown-item" href="{{route('create_ram')}}">RAM</a>
+                <a class="dropdown-item" href="{{route('create_gpu')}}">GPU</a>
+                <a class="dropdown-item" href="{{route('create_storage')}}">SSD/HDD</a>
+                <a class="dropdown-item" href="{{route('create_psu')}}">PSU</a>
+                <a class="dropdown-item" href="{{route('create_mouse')}}">Mouse</a>
+                <a class="dropdown-item" href="{{route('create_keyboard')}}">Keyboard</a>
+                <a class="dropdown-item" href="{{route('create_headphone')}}">Headphone</a>
+            </div>
+        </div>
+<h2 class="text-center">UPDATE NEW MOTHERBOARD </h1>
+<div class="container">
+    <form action="{{URL::to('admin/save_product')}}" method="post" enctype="multipart/form-data">
+        @csrf
+        <div class="row">
+            <div class="col">
+                <div class="form-group">
+                    Name
+                    <input type="text" name="product_name" class="form-control" style="width:350px">
+                </div>           
+                <div class="form-group">
+                        Category
+                        <select name="category" class="form-control"style="width:200px">
+                            <option value="">-----Choose-----</option>
+                            @foreach($category as $cate)
+                            <option value="{{$cate->category_id}}">{{$cate->category_name}}</option>
+                            @endforeach
+                            {{-- {!! $htmlOption !!} --}}
+                        </select>
+                </div>
+                <div class="form-group" >
+                    Brand
+                    <select name="brand" class="form-control"style="width:200px">
+                        <option value="">-----Choose-----</option>
+                        @foreach($brand as $key=>$bra)
+                            <option value="{{$bra->brand_id}}"> {{$bra->brand_name}}</option>
+                        @endforeach
+                    </select>
+                </div>
+                 <div class="form-group">
+                    Supplier
+                    <select name="supplier" class="form-control"style="width:200px">
+                        <option value="">-----Choose-----</option>
+                        @foreach($supplier as $key=>$sup)
+                            <option value="{{$sup->supplier_id}}"> {{$sup->supplier_name}}</option>
+                        @endforeach
+                     </select>
+                </div>
+                {{-- spec type --}}
+                {{-- <input type="hidden" name="component" value ="Ram"> --}}
+                <div class="form-group">
+                    Size
+                    <select name="motherboard_size" class="form-control"style="width:200px">
+                        <option value="ATX">ATX</option>
+                        <option value="MicroATX">MicroATX</option>
+                        <option value="Mini-ITX">Mini-ITX</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    Socket
+                   <select name="motherboard_socket" class="form-control"style="width:200px">
+                    <optgroup label="Intel">Intel
+                        <option value="LGA 1151">LGA 1151</option>
+                        <option value="LGA 1366">LGA 1366</option>
+                        <option value="LGA1200">LGA1200</option>
+                    </optgroup>
+                    <optgroup label="AMD">AMD
+                        <option value="AM2">AM2</option>
+                        <option value="AM2+">AM2+</option>
+                        <option value="AM3">AM3</option>
+                        <option value="AM4">AM4</option>
+                    </optgroup>
+                   </select>
+               </div>
+                <div class="form-group">
+                    Chipset
+                   <select name="motherboard_chipset" class="form-control"style="width:200px">
+                    <option value="Intel">Intel</option>
+                    <option value="AMD">AMD</option>
+                   </select>
+               </div>
+                {{-- end spec type --}}
+            </div>
+            <div class="col">
+                <div class="form-group">
+                    Price
+                    <input type="text" name="product_price" class="form-control" style="width:350px">
+                </div>
+                <div class="form-group" >
+                    SKU
+                    <input type="text" name="product_sku" class="form-control" style="width:350px">
+                </div>
+                Description
+                <div class="form-group">
+                    <textarea   rows="5" cols="60" name="product_description"></textarea>
+                </div>
+                Sort Description
+                <div class="form-group">
+                    <textarea   rows="5" cols="60" name="product_sort_description"></textarea>
+                </div>
+                <div class="form-group">
+                    Main Image
+                    <input type="file" name="product_image_main" class="form-control" style="width:350px">
+                </div>
+                <div class="form-group">
+                    Gallery Image
+                    <input type="file" name="product_image_gallery" class="form-control" style="width:350px">
+                </div>
+            </div>
+        </div>
+        
+        <input style="margin-top:20px" type="submit" value="Create" class="btn btn-info btn-lg btn-block" name="create_cate">
+    </form>
+</div>
+@endsection
---
+++ Novatek/resources/views/admin/product/form_update/psu.blade.php
@@ -0,0 +1,127 @@
+@extends('admin.admin_layout')
+@section('admin-content')
+        <div class="dropdown show ">
+            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Choose component
+            </a>
+          
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                <a class="dropdown-item" href="{{route('create_motherboard')}}">Motherboard</a>
+                <a class="dropdown-item" href="{{route('create_cpu')}}">CPU</a>
+                <a class="dropdown-item" href="{{route('create_ram')}}">RAM</a>
+                <a class="dropdown-item" href="{{route('create_gpu')}}">GPU</a>
+                <a class="dropdown-item" href="{{route('create_storage')}}">SSD/HDD</a>
+                <a class="dropdown-item" href="{{route('create_psu')}}">PSU</a>
+                <a class="dropdown-item" href="{{route('create_mouse')}}">Mouse</a>
+                <a class="dropdown-item" href="{{route('create_keyboard')}}">Keyboard</a>
+                <a class="dropdown-item" href="{{route('create_headphone')}}">Headphone</a>
+            </div>
+        </div>
+<h2 class="text-center">UPDATE NEW PSU </h1>
+<div class="container">
+    <form action="{{URL::to('admin/save_product')}}" method="post" enctype="multipart/form-data">
+        @csrf
+        <div class="row">
+            <div class="col">
+                <div class="form-group">
+                    Name
+                    <input type="text" name="product_name" class="form-control" style="width:350px">
+                </div>           
+                <div class="form-group">
+                        Category
+                        <select name="category" class="form-control"style="width:200px">
+                            <option value="">-----Choose-----</option>
+                            @foreach($category as $cate)
+                            <option value="{{$cate->category_id}}">{{$cate->category_name}}</option>
+                            @endforeach
+                            {{-- {!! $htmlOption !!} --}}
+                        </select>
+                </div>
+                <div class="form-group" >
+                    Brand
+                    <select name="brand" class="form-control"style="width:200px">
+                        <option value="">-----Choose-----</option>
+                        @foreach($brand as $key=>$bra)
+                            <option value="{{$bra->brand_id}}"> {{$bra->brand_name}}</option>
+                        @endforeach
+                    </select>
+                </div>
+                 <div class="form-group">
+                    Supplier
+                    <select name="supplier" class="form-control"style="width:200px">
+                        <option value="">-----Choose-----</option>
+                        @foreach($supplier as $key=>$sup)
+                            <option value="{{$sup->supplier_id}}"> {{$sup->supplier_name}}</option>
+                        @endforeach
+                     </select>
+                </div>
+                {{-- spec type --}}
+                {{-- <input type="hidden" name="component" value ="Ram"> --}}
+                <div class="form-group">
+                    Type
+                    <select name="psu_type" class="form-control"style="width:200px">
+                        <option value="LINEAR">LINEAR</option>
+                        <option value="SWITCHED">SWITCHED</option>
+                        <option value="BATTERY-BASE">BATTERY-BASE</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    Power
+                   <select name="psu_power" class="form-control"style="width:200px">
+                       <option value="120W">120W</option>
+                       <option value="200W">200W</option>
+                       <option value="400W">400W</option>
+                       <option value="450W">450W</option>
+                       <option value="500W">500W</option>
+                       <option value="600W">600W</option>
+                       <option value="650W">650W</option>
+                       <option value="700W">700W</option>
+                       <option value="750W">750W</option>
+                       <option value="1000W">1000W</option>
+                       <option value="1200W">1200W</option>
+                       <option value="1250W">1250W</option>
+                   </select>
+               </div>
+               <div class="form-group">
+                    Efficiency
+                    <select name="psu_efficiency" class="form-control"style="width:200px">
+                        <option value="80 PLUS">80 PLUS</option>
+                        <option value="80 PLUS BROZEN">80 PLUS BROZEN</option>
+                        <option value="80 PLUS SLIVER">80 PLUS SLIVER</option>
+                        <option value="80 PLUS GOLD">80 PLUS GOLD</option>
+                    </select>
+                </div>
+                {{-- end spec type --}}
+            </div>
+            <div class="col">
+                <div class="form-group">
+                    Price
+                    <input type="text" name="product_price" class="form-control" style="width:350px">
+                </div>
+                <div class="form-group" >
+                    SKU
+                    <input type="text" name="product_sku" class="form-control" style="width:350px">
+                </div>
+                Description
+                <div class="form-group">
+                    <textarea   rows="5" cols="60" name="product_description"></textarea>
+                </div>
+                Sort Description
+                <div class="form-group">
+                    <textarea   rows="5" cols="60" name="product_sort_description"></textarea>
+                </div>
+                <div class="form-group">
+                    Main Image
+                    <input type="file" name="product_image_main" class="form-control" style="width:350px">
+                </div>
+                <div class="form-group">
+                    Gallery Image
+                    <input type="file" name="product_image_gallery" class="form-control" style="width:350px">
+                </div>
+            </div>
+        </div>
+        
+        <input style="margin-top:20px" type="submit" value="Create" class="btn btn-info btn-lg btn-block" name="create_cate">
+    </form>
+</div>
+@endsection
