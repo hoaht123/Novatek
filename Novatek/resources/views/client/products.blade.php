94432fbb415b6c34210b72a773ee1c816025b2e2
Van Tinh <cftq0007@gmail.com>
2022-03-12 16:21:19 +0700

    view product

--- Novatek/resources/views/client/products.blade.php
+++ Novatek/resources/views/client/products.blade.php
@@ -117,120 +117,92 @@
             </div>
             <div class="row">
                 <div class="col-sm-6 col-md-3 col-xs-b25">
-                    <div class="h4 col-xs-b25">Hot Sale</div>
+                    @php
+                        $hots = DB::select('select product.product_id, AVG(invoice_details.quantity) as quantity from product  join invoice_details on product.product_id = invoice_details.product_id group by product.product_id order by quantity desc limit 3');
+                        $id = array();
+                        for ($i=0; $i < count($hots); $i++) {
+                            $id[$i] = $hots[$i]->product_id;
+                        }
+                        $hotSales = DB::table('product')->whereIn('product_id',$id)->get();
+                    @endphp
+                    <div class="h4 col-xs-b25">Hot Sales</div>
+                    @foreach($hotSales as $product)
                     <div class="product-shortcode style-4 rounded clearfix">
-                        <a class="preview" href="#"><img src="{{ asset('client/img/product-28.jpg')}}" alt="" /></a>
+                        <a class="preview" href="{{ route('client.product_detail',['product_id' => $product->product_id])}}"><img src="{{ asset('images/product/'.$product->product_main_image)}}" alt="" /></a>
                         <div class="description">
-                            <div class="simple-article color size-1 col-xs-b5"><a href="#">WIRELESS</a></div>
-                            <h6 class="h6 col-xs-b10"><a href="#">wireless headphones</a></h6>
-                            <div class="simple-article dark">$98.00</div>
+                            <div class="simple-article color size-1 col-xs-b5"><a href="{{ route('client.product_detail',['product_id' => $product->product_id])}}">{{$product->component}}</a></div>
+                            <h6 class="h6 col-xs-b10"><a href="{{ route('client.product_detail',['product_id' => $product->product_id])}}">{{$product->product_name}}</a></h6>
+                            <div class="simple-article dark">${{$product->product_price}}.00</div>
                         </div>
                     </div>
                     <div class="col-xs-b10"></div>
-                    <div class="product-shortcode style-4 rounded clearfix">
-                        <a class="preview" href="#"><img src="{{ asset('client/img/product-29.jpg')}}" alt="" /></a>
-                        <div class="description">
-                            <div class="simple-article color size-1 col-xs-b5"><a href="#">CASES</a></div>
-                            <h6 class="h6 col-xs-b10"><a href="#">earphones case</a></h6>
-                            <div class="simple-article dark">$12.00</div>
-                        </div>
-                    </div>
-                    <div class="col-xs-b10"></div>
-                    <div class="product-shortcode style-4 rounded clearfix">
-                        <a class="preview" href="#"><img src="{{ asset('client/img/product-30.jpg')}}" alt="" /></a>
-                        <div class="description">
-                            <div class="simple-article color size-1 col-xs-b5"><a href="#">CASES</a></div>
-                            <h6 class="h6 col-xs-b10"><a href="#">headphones case</a></h6>
-                            <div class="simple-article"><span class="color">$24.00</span>&nbsp;&nbsp;&nbsp;<span class="line-through">$32.00</span></div>
-                        </div>
-                    </div>
+                    @endforeach
                 </div>
                 <div class="col-sm-6 col-md-3 col-xs-b25">
+                    @php
+                        $ratings = DB::select('select product.product_id, AVG(review.rating) as rating from product  join review on product.product_id = review.product_id group by product.product_id order by rating desc limit 3');
+                        $id = array();
+                        for ($i=0; $i < count($ratings); $i++) {
+                            $id[$i] = $ratings[$i]->product_id;
+                        }
+                        $topRating = DB::table('product')->whereIn('product_id',$id)->get();
+                    @endphp
                     <div class="h4 col-xs-b25">Top Rated</div>
+                    @foreach ($topRating as $product)
                     <div class="product-shortcode style-4 rounded clearfix">
-                        <a class="preview" href="#"><img src="{{ asset('client/img/product-31.jpg')}}" alt="" /></a>
+                        <a class="preview" href="{{ route('client.product_detail',['product_id' => $product->product_id])}}"><img src="{{ asset('images/product/'.$product->product_main_image)}}" alt="" /></a>
                         <div class="description">
-                            <div class="simple-article color size-1 col-xs-b5"><a href="#">WIRELESS</a></div>
-                            <h6 class="h6 col-xs-b10"><a href="#">wireless headphones</a></h6>
-                            <div class="simple-article dark">$98.00</div>
+                            <div class="simple-article color size-1 col-xs-b5"><a href="{{ route('client.product_detail',['product_id' => $product->product_id])}}">{{$product->component}}</a></div>
+                            <h6 class="h6 col-xs-b10"><a href="{{ route('client.product_detail',['product_id' => $product->product_id])}}">{{$product->product_name}}</a></h6>
+                            <div class="simple-article dark">${{$product->product_price}}.00</div>
                         </div>
                     </div>
                     <div class="col-xs-b10"></div>
-                    <div class="product-shortcode style-4 rounded clearfix">
-                        <a class="preview" href="#"><img src="{{ asset('client/img/product-32.jpg')}}" alt="" /></a>
-                        <div class="description">
-                            <div class="simple-article color size-1 col-xs-b5"><a href="#">CASES</a></div>
-                            <h6 class="h6 col-xs-b10"><a href="#">earphones case</a></h6>
-                            <div class="simple-article dark">$12.00</div>
-                        </div>
-                    </div>
-                    <div class="col-xs-b10"></div>
-                    <div class="product-shortcode style-4 rounded clearfix">
-                        <a class="preview" href="#"><img src="{{ asset('client/img/product-33.jpg')}}" alt="" /></a>
-                        <div class="description">
-                            <div class="simple-article color size-1 col-xs-b5"><a href="#">CASES</a></div>
-                            <h6 class="h6 col-xs-b10"><a href="#">headphones case</a></h6>
-                            <div class="simple-article dark">$4.00</div>
-                        </div>
-                    </div>
+                    @endforeach
                 </div>
                 <div class="col-sm-6 col-md-3 col-xs-b25">
-                    <div class="h4 col-xs-b25">Popular</div>
+                    @php
+                        // $key = DB::select('select product.product_id, AVG(invoice_details.quantity) as quantity from product  join invoice_details on product.product_id = invoice_details.product_id group by product.product_id order by quantity desc limit 3');
+                        // $id = array();
+                        // for ($i=0; $i < count($key); $i++) {
+                        //     $id[$i] = $key[$i]->product_id;
+                        // }
+                        $popular = DB::select(' select * from product where product_price between 100 and 200 order by product_price limit 3');
+                    @endphp
+                    <div class="h4 col-xs-b25">popular</div>
+                    @foreach($popular as $product)
                     <div class="product-shortcode style-4 rounded clearfix">
-                        <a class="preview" href="#"><img src="{{ asset('client/img/product-34.jpg')}}" alt="" /></a>
+                        <a class="preview" href="{{ route('client.product_detail',['product_id' => $product->product_id])}}"><img src="{{ asset('images/product/'.$product->product_main_image)}}" alt="" /></a>
                         <div class="description">
-                            <div class="simple-article color size-1 col-xs-b5"><a href="#">WIRELESS</a></div>
-                            <h6 class="h6 col-xs-b10"><a href="#">wireless headphones</a></h6>
-                            <div class="simple-article dark">$98.00</div>
+                            <div class="simple-article color size-1 col-xs-b5"><a href="{{ route('client.product_detail',['product_id' => $product->product_id])}}">{{$product->component}}</a></div>
+                            <h6 class="h6 col-xs-b10"><a href="{{ route('client.product_detail',['product_id' => $product->product_id])}}">{{$product->product_name}}</a></h6>
+                            <div class="simple-article dark">${{$product->product_price}}.00</div>
                         </div>
                     </div>
                     <div class="col-xs-b10"></div>
-                    <div class="product-shortcode style-4 rounded clearfix">
-                        <a class="preview" href="#"><img src="{{ asset('client/img/product-35.jpg')}}" alt="" /></a>
-                        <div class="description">
-                            <div class="simple-article color size-1 col-xs-b5"><a href="#">CASES</a></div>
-                            <h6 class="h6 col-xs-b10"><a href="#">earphones case</a></h6>
-                            <div class="simple-article dark">$12.00</div>
-                        </div>
-                    </div>
-                    <div class="col-xs-b10"></div>
-                    <div class="product-shortcode style-4 rounded clearfix">
-                        <a class="preview" href="#"><img src="{{ asset('client/img/product-36.jpg')}}" alt="" /></a>
-                        <div class="description">
-                            <div class="simple-article color size-1 col-xs-b5"><a href="#">CASES</a></div>
-                            <h6 class="h6 col-xs-b10"><a href="#">headphones case</a></h6>
-                            <div class="simple-article dark">$4.00</div>
-                        </div>
-                    </div>
+                    @endforeach
                 </div>
                 <div class="col-sm-6 col-md-3 col-xs-b25">
+                    @php
+                        $choices = DB::select('select product.product_id, AVG(invoice_details.quantity) as quantity from product  join invoice_details on product.product_id = invoice_details.product_id group by product.product_id order by quantity desc limit 3');
+                        $id = array();
+                        for ($i=0; $i < count($choices); $i++) {
+                            $id[$i] = $choices[$i]->product_id;
+                        }
+                        $bestChoices = DB::table('product')->whereIn('product_id',$id)->get();
+                    @endphp
                     <div class="h4 col-xs-b25">Best Choice</div>
+                    @foreach($bestChoices as $product)
                     <div class="product-shortcode style-4 rounded clearfix">
-                        <a class="preview" href="#"><img src="{{ asset('client/img/product-37.jpg')}}" alt="" /></a>
+                        <a class="preview" href="{{ route('client.product_detail',['product_id' => $product->product_id])}}"><img src="{{ asset('images/product/'.$product->product_main_image)}}" alt="" /></a>
                         <div class="description">
-                            <div class="simple-article color size-1 col-xs-b5"><a href="#">WIRELESS</a></div>
-                            <h6 class="h6 col-xs-b10"><a href="#">wireless headphones</a></h6>
-                            <div class="simple-article dark">$98.00</div>
+                            <div class="simple-article color size-1 col-xs-b5"><a href="{{ route('client.product_detail',['product_id' => $product->product_id])}}">{{$product->component}}</a></div>
+                            <h6 class="h6 col-xs-b10"><a href="{{ route('client.product_detail',['product_id' => $product->product_id])}}">{{$product->product_name}}</a></h6>
+                            <div class="simple-article dark">${{$product->product_price}}.00</div>
                         </div>
                     </div>
                     <div class="col-xs-b10"></div>
-                    <div class="product-shortcode style-4 rounded clearfix">
-                        <a class="preview" href="#"><img src="{{ asset('client/img/product-38.jpg')}}" alt="" /></a>
-                        <div class="description">
-                            <div class="simple-article color size-1 col-xs-b5"><a href="#">CASES</a></div>
-                            <h6 class="h6 col-xs-b10"><a href="#">earphones case</a></h6>
-                            <div class="simple-article dark">$12.00</div>
-                        </div>
-                    </div>
-                    <div class="col-xs-b10"></div>
-                    <div class="product-shortcode style-4 rounded clearfix">
-                        <a class="preview" href="#"><img src="{{ asset('client/img/product-39.jpg')}}" alt="" /></a>
-                        <div class="description">
-                            <div class="simple-article color size-1 col-xs-b5"><a href="#">CASES</a></div>
-                            <h6 class="h6 col-xs-b10"><a href="#">headphones case</a></h6>
-                            <div class="simple-article dark">$4.00</div>
-                        </div>
-                    </div>
+                    @endforeach
                 </div>
             </div>
     </div>
